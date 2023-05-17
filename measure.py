6e9a9f62cac04843733c387a07a04dfed1ea66aa
leejheth <leejih@ethz.ch>
2023-05-17 23:54:21 +0200

    fixed NACK and timeout issue, SQL data type issue

--- measure.py
+++ measure.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# (c) Copyright 2023 Sensirion AG, Switzerland
+# Adapted by Jihyun Lee for SQL database upload
+
 from sensirion_shdlc_driver import ShdlcSerialPort, ShdlcConnection
 from sensirion_shdlc_sensorbridge import SensorBridgePort, SensorBridgeShdlcDevice, SensorBridgeI2cProxy
-from sensirion_i2c_driver import I2cConnection, I2cDevice
+from sensirion_i2c_driver import I2cConnection
 from sensirion_i2c_scd import Scd4xI2cDevice
 
 import mysql.connector 
@@ -22,7 +28,7 @@ def upload_SQL(co2, temperature, humidity, sql_credentials_all):
         cur = conn.cursor()
         tablename = sql_credentials[5]
         query = f"INSERT INTO {tablename} (CO2, temperature, humidity, time) VALUES (%s, %s, %s, %s)"
-        timestamp = datetime.datetime.now()
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
         cur.execute(query, (co2,temperature,humidity,timestamp))
         conn.commit()
         cur.close()
@@ -43,30 +49,36 @@ with open('conf/mysql.txt', 'r') as f:
 #  - slave address: 0
 with ShdlcSerialPort(port='COM6', baudrate=460800) as port:
     bridge = SensorBridgeShdlcDevice(ShdlcConnection(port), slave_address=0)
-    print("SensorBridge SN: {bridge.get_serial_number()}")
-    
+    print("SensorBridge SN: {}".format(bridge.get_serial_number()))
+
     # Configure SensorBridge port 1 for SCD4x
     bridge.set_i2c_frequency(SensorBridgePort.ONE, frequency=100e3)
     bridge.set_supply_voltage(SensorBridgePort.ONE, voltage=3.3)
     bridge.switch_supply_on(SensorBridgePort.ONE)
 
-    # Create SCD41 device
+    # Create SCD4x device
     i2c_transceiver = SensorBridgeI2cProxy(bridge, port=SensorBridgePort.ONE)
-    scd41 = Scd4xI2cDevice(I2cConnection(i2c_transceiver))
+    scd4x = Scd4xI2cDevice(I2cConnection(i2c_transceiver))
+
+    # Make sure measurement is stopped, else we can't read serial number or
+    # start a new measurement
+    scd4x.stop_periodic_measurement()
+
+    print("scd4x Serial Number: {}".format(scd4x.read_serial_number()))
 
     # start periodic measurement in high power mode
-    scd41.start_periodic_measurement()
+    scd4x.start_periodic_measurement()
 
     # Measure every 60 seconds
     while True:
         time.sleep(60)
-        co2, temperature, humidity = scd41.read_measurement()
+        co2, temperature, humidity = scd4x.read_measurement()
 
-        # print the output:
+        # print the output
         print(f"{co2}, {temperature}, {humidity}")
 
         # upload data to MySQL database
-        upload_SQL(round(co2), round(temperature), round(humidity), sql_credentials_all)
+        upload_SQL(f"{co2}", f"{temperature}", f"{humidity}", sql_credentials_all)
 
     scd41.stop_periodic_measurement()
 
